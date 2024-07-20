a88ad33ad02252d7f5787f7b51b2884b94ab4a3b
an-na-kuzmina <anna.bychkova2008@gmail.com>
2024-07-21 02:48:06 +0300

    finish timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -17,6 +17,60 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+const inputTimePicker = document.querySelector('input#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const dataDays = document.querySelector('span[data-days]');
+const dataHours = document.querySelector('span[data-hours]');
+const dataMinutes = document.querySelector('span[data-minutes]');
+const dataSeconds = document.querySelector('span[data-seconds]');
+let userSelectedDate;
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate.getTime() < Date.now()) {
+      startBtn.disabled = true;
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        timeout: 2000,
+      });
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(inputTimePicker, options);
+startBtn.addEventListener('click', startBtnClikHandler);
+
+function startBtnClikHandler(event) {
+  startBtn.disabled = true;
+  inputTimePicker.disabled = true;
+  const intervalId = setInterval(() => {
+    const dateDiff = userSelectedDate - Date.now();
+    if (dateDiff <= 0) {
+      inputTimePicker.disabled = false;
+      return clearInterval(intervalId);
+    }
+    const convertedDateDiff = convertMs(dateDiff);
+    updatedTimer(convertedDateDiff);
+  }, 1000);
+}
+
+function updatedTimer({ days, hours, minutes, seconds }) {
+  dataDays.textContent = String(days).padStart(2, 0);
+  dataHours.textContent = String(hours).padStart(2, 0);
+  dataMinutes.textContent = String(minutes).padStart(2, 0);
+  dataSeconds.textContent = String(seconds).padStart(2, 0);
+}
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -35,7 +89,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
